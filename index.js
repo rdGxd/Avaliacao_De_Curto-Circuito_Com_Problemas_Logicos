21a7167a5479b9887e4d41159be30a67188eb714
Rodrigo <rdg.hora1@gmail.com>
2023-10-17 12:24:42 -0300

    OR_AND

---
+++ index.js
@@ -0,0 +1,25 @@
+/*
+JavaScript: && - Python: (and) -> Retornar o primeiro "valor falso", se existir uma expressão falsa nas expressões. Se todas as expressões forem verdadeiras, retorna o valor da última expressão.
+
+JavaScript: || - Python: (or) -> Retornar o primeiro "valor verdadeiro", se existir uma expressão verdadeira nas expressões. Se todas as expressões forem falsas, retorna o valor da última expressão.
+*/
+
+/* OR
+const corPadrao = "black";
+let corUsuario = null || corPadrao;
+*/
+
+/* AND
+const nome = "Rodrigo";
+
+const sobrenome = "Silva";
+const falaNome = () => {
+  console.log(`${nome} ${sobrenome}`);
+};
+
+nome && sobrenome && falaNome();
+
+if (nome && sobrenome) {
+  falaNome();
+}
+ */
